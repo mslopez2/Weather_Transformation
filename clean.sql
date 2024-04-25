a6c29869f8c415c6ba4ce94219bae688c3f567eb
mslopez2 <139020351+mslopez2@users.noreply.github.com>
2024-04-24 21:12:40 -0700

    clean.sql

--- clean.sql
+++ clean.sql
@@ -1,9 +1,32 @@
-BEGIN
+BEGIN;
 
 
+INSERT INTO PDX_weather
+SELECT
+	id , 
+	(raw_json -> 'current' -> 'precip')::int AS precip,
+	(raw_json -> 'current' -> 'humidity')::int AS humidity,
+	(raw_json -> 'current' -> 'temperature')::int AS temp_c,
+	(raw_json -> 'current' -> 'cloudcover')::int AS cloudcover,
+	time
+FROM weather_scraper;
 
 
+UPDATE PDX_weather
+SET temp_f = (temp_C * 9/5)+32;
 
+
+UPDATE PDX_weather
+SET 
+	dat = time::date,
+	tim = time::time;
+
+
+DELETE FROM PDX_weather
+WHERE dat is null;
+
+
+DELETE FROM weather_scraper;
 
 
 COMMIT;
